858cbc3f37ac59a11393ad64bfd224c3d9a24b58
Alex Smith <alex@alexsmith.dev>
2021-05-13 21:04:57 +0100

    Add file metadata to return

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Arg, App};
 use std::fs;
 use std::fs::DirEntry;
+use std::time::SystemTime;
 use serde_json::json;
 
 fn main() {
@@ -17,16 +18,32 @@ fn main() {
     let entries = fs::read_dir(path).unwrap();
     for entry in entries {
         let path = entry.unwrap();
-        println!("{}", Ok(path.file_name().into_string()));
         println!("{}", entry_to_json(path));
     }
 }
 
 fn entry_to_json(entry: DirEntry) -> String {
     let metadata = entry.metadata().unwrap();
+     // Handle cases where there might be an error
+    let file_name = match entry.file_name().into_string() {
+        Ok(f) => f,
+        Err(_) => "".to_string()
+    };
+    let last_modified = match metadata.modified() {
+        Ok(t) => t,
+        Err(_) => SystemTime::UNIX_EPOCH
+    };
+    let created = match metadata.created() {
+        Ok(t) => t,
+        Err(_) => SystemTime::UNIX_EPOCH
+    };
     let result = json!({
-        "file_name": entry.file_name(),
-        "is_dir": metadata.is_dir()
+        "file_name": file_name,
+        "is_dir": metadata.is_dir(),
+        "size": metadata.len(),
+        "read_only": metadata.permissions().readonly(),
+        "last_modified": last_modified,
+        "created": created,
     });
 
     result.to_string()
