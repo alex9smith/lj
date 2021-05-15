f47abdd2abaddc22666609202404e01deeb0ac40
Alex Smith <alex@alexsmith.dev>
2021-05-15 14:07:02 +0100

    Add option to recursively search directories
    Pass `-r` to recursively search directories at the target path for
    files up to a maximum search depth. Set the depth with '-d=', defaults
    to 5.
    
    Also correct typo in README

--- README.md
+++ README.md
@@ -5,7 +5,18 @@ Like `ls` but outputs JSON!
 ## Usage
 
 ```bash
-$ jq [path] [options]
+$ lj [path] [options]
+```
+
+### Options
+```
+--help              Shows the available options.
+
+-r, --recursive     If set, lj will recursively search directories at the target path for files
+                    up to a maximum search depth. Set the depth with '-d=', defaults to 5.
+                    
+-d, --depth         The search depth to use when recursively searching folders for files.
+                    If set, you must also pass '-r', Defaults to 5.   
 ```
 
 ## Development
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate clap;
+
 use std::fs;
 use std::fs::{DirEntry, ReadDir};
 use std::time::SystemTime;
-use serde_json::json;
+use std::process;
+use serde_json::{json, to_value, Value};
 use clap::{Arg, App};
 
 fn main() {
@@ -11,26 +15,45 @@ fn main() {
         .arg(Arg::with_name("path")
              .help("The file path to list. Defaults to ./")
              .index(1))
+        .arg(Arg::with_name("recursive")
+            .short("r")
+            .long("recursive")
+            .help(
+                "If set, lj will recursively search directories at the target path for files
+                up to a maximum search depth. Set the depth with '-d=', defaults to 5"
+            ))
+        .arg(Arg::with_name("depth")
+            .short("d")
+            .takes_value(true)
+            .help("The search depth to use when recursively searching folders for files.
+                  If set, you must also pass '-r', Defaults to 5."))
         .get_matches();
 
-
+    // Parse arguments
     let path = matches.value_of("path").unwrap_or("./");
-    let json = dir_to_json(fs::read_dir(path).unwrap());
+    let recursive = matches.is_present("recursive");
+    if !recursive & matches.is_present("depth") {
+        eprintln!("Error: You must set '-r' when passing a depth!");
+        process::exit(1);
+    }
+    let depth = value_t!(matches.value_of("depth"), u32).unwrap_or(5);
+
+    let json = dir_to_json(fs::read_dir(path).unwrap(), recursive, depth, 0);
     for entry in json {
         println!("{}", entry)
     }
 }
 
-fn dir_to_json(dir: ReadDir) -> Vec<String> {
-    let mut json: Vec<String> = Vec::new();
+fn dir_to_json(dir: ReadDir, recursive: bool, max_depth: u32, current_depth: u32) -> Vec<Value> {
+    let mut json: Vec<Value> = Vec::new();
     for entry in dir {
         let path = entry.unwrap();
-        json.push(entry_to_json(path))
+        json.push(entry_to_json(path, recursive, max_depth, current_depth))
     }
     json
 }
 
-fn entry_to_json(entry: DirEntry) -> String {
+fn entry_to_json(entry: DirEntry, recursive: bool, max_depth: u32, current_depth: u32) -> Value {
     let metadata = entry.metadata().unwrap();
      // Handle cases where there might be an error
     let file_name = match entry.file_name().into_string() {
@@ -45,7 +68,7 @@ fn entry_to_json(entry: DirEntry) -> String {
         Ok(t) => t,
         Err(_) => SystemTime::UNIX_EPOCH
     };
-    let result = json!({
+    let mut result = json!({
         "file_name": file_name,
         "is_dir": metadata.is_dir(),
         "size": metadata.len(),
@@ -54,5 +77,14 @@ fn entry_to_json(entry: DirEntry) -> String {
         "created": created,
     });
 
-    result.to_string()
+    if metadata.is_dir() & (current_depth < max_depth) & recursive {
+        //TODO - read the folder properly and pass it in
+        let full_path = fs::canonicalize(entry.path()).unwrap();
+        result["contents"] = match to_value(dir_to_json(fs::read_dir(full_path).unwrap(), recursive, max_depth, current_depth + 1)) {
+            Ok(t) => t,
+            Err(_) => Value::Array(Vec::new())
+        }
+    }
+
+    result
 }
